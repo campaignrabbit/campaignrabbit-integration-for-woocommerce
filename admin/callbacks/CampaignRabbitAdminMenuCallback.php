92cbf41209fc5efb59b4eee36cc6bd336ce5821e
Prashanthi Ramesh <rvprashanthi@gmail.com>
2018-06-27 01:21:05 +0530

    - sync display

--- admin/callbacks/CampaignRabbitAdminMenuCallback.php
+++ admin/callbacks/CampaignRabbitAdminMenuCallback.php
@@ -25,6 +25,8 @@ class CampaignRabbitAdminMenuCallback
 
         $first_migrate = get_option('first_migrate');
         if ($authenticated) {
+            ?>  <br> <h2><?php esc_attr_e("CampaignRabbit Synchronization", "campaignrabbit-integration-for-woocommerce"); ?></h2>
+            <?php
             $this->displayDisabledCron();
             if (!$first_migrate) {
                 if (wp_get_schedule('campaignrabbit_recurring_bulk_migration')) {
@@ -61,7 +63,7 @@ class CampaignRabbitAdminMenuCallback
         $diff = strtotime($next_run) - strtotime($now);
         if ($diff < 0) {
             ?>
-            <h3><?php esc_html_e('Cron Event campaignrabbit_recurring_bulk_migration has ended. Please try again!', 'campaignrabbit-integration-for-woocommerce') ?></h3>
+            <h4><?php esc_html_e('Cron Event campaignrabbit_recurring_bulk_migration has ended. Please try again!', 'campaignrabbit-integration-for-woocommerce') ?></h4>
             <?php
         } else {
             $minutes = floor(($diff) / (60));
@@ -101,7 +103,7 @@ class CampaignRabbitAdminMenuCallback
             <input type="hidden" name="action" value="update_campaignrabbit_settings"/>
             <input type="hidden" name="hidden_api_token_flag" id="hidden_api_token_flag"
                    value="<?php echo get_option('api_token_flag') ?>"/>
-            <h3><?php esc_attr_e("CampaignRabbit Authentication Information", "campaignrabbit-integration-for-woocommerce"); ?></h3>
+            <h2><?php esc_attr_e("CampaignRabbit Authentication Information", "campaignrabbit-integration-for-woocommerce"); ?></h2>
             <p>
                 <label class="woo-label"><?php esc_attr_e("API Token:", "campaignrabbit-integration-for-woocommerce"); ?></label>
                 <input type="text" size="80" name="api_token" id="api_token"
@@ -120,6 +122,14 @@ class CampaignRabbitAdminMenuCallback
     }
 
     private function displayResync(){
-
+        ?>
+        <br>
+        <form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
+            <input type="hidden" name="action" value="resync_migration"/>
+            <label><?php esc_html_e("Re-Sync the recurring migration of customers,products,orders from woocommerce to CampaignRabbit- "); ?></label>
+            <input name="woo_connect" id="woo_connect" class="button button-primary" type="submit"
+                   value="<?php esc_attr_e("Re-Sync", "campaignrabbit-integration-for-woocommerce"); ?>"/>
+        </form>
+        <?php
     }
 }
